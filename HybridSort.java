219eac9317a227de499a7881a2668000e88bc39b
MustafaJP <MUSTAFAJ001@e.ntu.edu.sg>
2023-09-12 18:29:53 +0800

    Hybrid Sort Complete but Key comparision IDK if working

--- HybridSort.java
+++ HybridSort.java
@@ -1,40 +1,48 @@
 import java.util.*;
 
-public class Mergesort
-{
-	int[] slot;
+public class HybridSort {
+	
+	int[] slot = {0, 0, 1, 3, 9, 4, 0, 1, 7, 1};
+	int S = 4;
+	int keyComp;
 
-	public Mergesort(){
-		slot = generateRandom(10, 10);
+	public HybridSort(){
+		// slot = generateRandom(10, 10);
+		keyComp = 0;
 	}
 	public static void main(String[] args)
 	{
-		Mergesort list = new Mergesort();
+		HybridSort list = new HybridSort();
     System.out.println("Before Merge Sort:");
     System.out.println(Arrays.toString(list.slot));
 
-    list.mergesort(0, 9); // Sort the entire array, assuming the array has 10 elements (indices 0 to 9).
+    list.hybridSorter(0, 9); // Sort the entire array, assuming the array has 10 elements (indices 0 to 9).
 
-    System.out.println("After Merge Sort:");
+    System.out.println("After Merge Sort wtih InsertSort:");
     System.out.println(Arrays.toString(list.slot));
+
+		System.out.println("Key Comparision - " + list.keyComp);
 	}
 
 	public static int[] generateRandom(int size, int max) {
         return new Random().ints(size, 0, max).toArray();
   }
 
-	void mergesort(int n, int m){
+	void hybridSorter(int n, int m){
 		int mid = (n+m)/ 2;
 		if (m-n <= 0) return;
 		else if (m-n > 1){
-			mergesort(n, mid);
-			mergesort(mid+1, m);
+			hybridSorter(n, mid);
+			hybridSorter(mid+1, m);
 		}
 		merge(n, m);
 	}
 
 	void merge(int n, int m)
 	{		
+		if (slot.length <= S){
+			insertionSort(slot.length);
+		}
 		int mid = (n+m)/2;
 		int a = n, b = mid+1, i, tmp;
 		if (m-n <= 0) return;
@@ -58,9 +66,28 @@ public class Mergesort
 	} // end of merge
 
 	int compare(int a, int b){
+		keyComp++;
 		if (a>b) return 1;
 		else if (a<b) return -1;
 		else return 0;
 	}
-	
+
+	void insertionSort (int n)
+	{ // input slot is an array of n records; // assume n > 1;
+		for (int i=1; i < n; i++)
+			for (int j=i; j > 0; j--) {
+				keyComp++;
+				if (slot[j] < slot[j-1])
+				{
+					swap(slot[j], slot[j-1], j); 
+				}
+			else break;
+		}
+	}
+	void swap(int n, int m, int index)
+	{
+		slot[index - 1] = n;
+		slot[index] = m;
+	}
+
 }
